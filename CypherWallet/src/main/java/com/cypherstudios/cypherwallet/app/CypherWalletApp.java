ded9a0f7da18982639e7a853fc0ad22b4a30cf82
Victor Visús <victor.vxg@gmail.com>
2021-08-09 20:39:46 +0200

    Actualizar CypherWalletApp.java y CtrlInicio.java

--- CypherWallet/src/main/java/com/cypherstudios/cypherwallet/app/CypherWalletApp.java
+++ CypherWallet/src/main/java/com/cypherstudios/cypherwallet/app/CypherWalletApp.java
@@ -1,6 +1,15 @@
 package com.cypherstudios.cypherwallet.app;
 
+import com.cypherstudios.cypherwallet.controller.CtrlInicio;
+
 public class CypherWalletApp {
+
+    //Controladores
+    /**
+     * Instancio la clase controlador del panel de Inicio de la app
+     *
+     */
+    public static CtrlInicio ctrlInicio = new CtrlInicio();
 
     public static void main(String[] args) {
 
---
+++ CypherWallet/src/main/java/com/cypherstudios/cypherwallet/controller/CtrlInicio.java
@@ -0,0 +1,10 @@
+package com.cypherstudios.cypherwallet.controller;
+
+/**
+ * Clase controladora del panel de Inicio de la app.
+ *
+ * @author Victor Visús García
+ */
+public class CtrlInicio {
+
+}
